d85726c7e747b2a39105c232ceb8f1bc0803090b
WilsonMolina <wilson.molina.f@gmail.com>
2017-12-11 19:19:57 -0300

    Mobile
    Ultimos ajustes mobile

--- js/src/main.js
+++ js/src/main.js
@@ -38,6 +38,37 @@ var App = {
 			
 			
 			
+			$('#bloque-iconos').slick({
+			  arrow: false,
+			  dots: true,
+			  infinite: false,
+			  speed: 300,
+			  slidesToShow: 4,
+			  slidesToScroll: 4,
+			  responsive: [
+			    {
+			      breakpoint: 1024,
+			      settings: {
+			        slidesToShow: 4,
+			        slidesToScroll: 4,
+			        infinite: true,
+			        dots: false
+			      }
+			    },
+			    {
+			      breakpoint: 480,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1
+			      }
+			    }
+			    // You can unslick at a given breakpoint now by adding:
+			    // settings: "unslick"
+			    // instead of a settings object
+			  ]
+			});
+			
+			
 				
 			
 			$('#pagepiling').pagepiling({
